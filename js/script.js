8631d5655a48e122f85c20d4bffe914113f9b5ed
ikhwnysrh <ikhwanyusrah@gmail.com>
2024-04-25 22:37:40 +0800

    fixing

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const inputFahrenheit = document.getElementById('fahrenheit');
 const inputKalkulasi = document.getElementById('kalkulasi');
 const tombolKonversi = document.querySelector('.btn button:nth-child(1)');
 const tombolReset = document.querySelector('.btn button:nth-child(2)');
-const tombolBalik = document.querySelector('.btn button:nth-child(3)');
+const tombolReverse = document.querySelector('.btn button:nth-child(3)');
 
 // Fungsi untuk mengonversi Celsius ke Fahrenheit
 function celsiusKeFahrenheit(celsius) {
@@ -16,30 +16,35 @@ function fahrenheitKeCelsius(fahrenheit) {
     return (fahrenheit - 32) * 5/9;
 }
 
-// Mendengarkan peristiwa untuk tombol konversi
+// Event listener untuk tombol konversi
 tombolKonversi.addEventListener('click', () => {
-    if (inputCelsius.value !== '') {
-        const celsius = parseFloat(inputCelsius.value);
-        const fahrenheit = celsiusKeFahrenheit(celsius);
-        inputFahrenheit.value = fahrenheit.toFixed(2);
-        inputKalkulasi.value = `${celsius}°C ke Fahrenheit: (${celsius} × 9/5) + 32 = ${fahrenheit}°F`;
-    } else if (inputFahrenheit.value !== '') {
-        const fahrenheit = parseFloat(inputFahrenheit.value);
-        const celsius = fahrenheitKeCelsius(fahrenheit);
-        inputCelsius.value = celsius.toFixed(2);
-        inputKalkulasi.value = `Fahrenheit ke ${celsius}°C: (${fahrenheit} - 32) × 5/9 = ${celsius}°C`;
+    if (inputCelsius.value !== '' || inputFahrenheit.value !== '') {
+        if (inputCelsius.value !== '') {
+            const celsius = parseFloat(inputCelsius.value);
+            const fahrenheit = celsiusKeFahrenheit(celsius);
+            inputFahrenheit.value = fahrenheit.toFixed(2);
+            inputKalkulasi.value = `${celsius}°C ke Fahrenheit: (${celsius} × 9/5) + 32 = ${fahrenheit}°F`;
+        } else if (inputFahrenheit.value !== '') {
+            const fahrenheit = parseFloat(inputFahrenheit.value);
+            const celsius = fahrenheitKeCelsius(fahrenheit);
+            inputCelsius.value = celsius.toFixed(2);
+            inputKalkulasi.value = `Fahrenheit ke ${celsius}°C: (${fahrenheit} - 32) × 5/9 = ${celsius}°C`;
+        }
+    } else {
+        alert("Masukkan nilai suhu terlebih dahulu!");
     }
 });
 
-// Mendengarkan peristiwa untuk tombol reset
+
+// Event listener untuk tombol reset
 tombolReset.addEventListener('click', () => {
     inputCelsius.value = '';
     inputFahrenheit.value = '';
     inputKalkulasi.value = '';
 });
 
-// Mendengarkan peristiwa untuk tombol balik
-tombolBalik.addEventListener('click', () => {
+// Event listener untuk tombol balik
+tombolReverse.addEventListener('click', () => {
     const temp = inputCelsius.value;
     inputCelsius.value = inputFahrenheit.value;
     inputFahrenheit.value = temp;
